d01648616aeb75c21d59daf8d4c130941667b856
olegische <to_snezhny@mail.ru>
2024-11-25 15:06:58 +0500

    fix: remove unsupported prop from ThemeProvider
    - Remove suppressHydrationWarning prop as it's not supported by next-themes
    - Fix type error in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-          suppressHydrationWarning
         >
           {children}
         </ThemeProvider>
